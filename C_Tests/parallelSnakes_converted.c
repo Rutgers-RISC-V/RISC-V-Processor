a5d0f3a1add7581406859d9069b9ac254eabc27f
Oz Bejerano <ob92@scarletmail.rutgers.edu>
2019-03-18 23:20:34 -0400

    Modified parallelSnakes_converted.c to be able to compiled on my (Oz's) machine.

--- C_Tests/parallelSnakes_converted.c
+++ C_Tests/parallelSnakes_converted.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include "terminal.h"
 
 int main(){
 	int offset = 0;
 	int printPosition = 0;
 	int unitChange = 1;
+	int a, y , x;
 
-	for(int a = 0; a < 10; a++){
-		for(int y=0; y<30; y++){
-			for(int x=0; x<80; x++){
+	for(a = 0; a < 10; a++){
+		for(y=0; y<30; y++){
+			for(x=0; x<80; x++){
 				printPosition = x + offset;
 				if(printPosition%5 == 0){
 					writeChar(y, x, '0');
@@ -22,8 +23,8 @@ int main(){
 			offset = offset + unitChange;
 		}
 
-		for(int y=0; y<30; y++){
-			for(int x=0; x<80; x++){
+		for(y=0; y<30; y++){
+			for(x=0; x<80; x++){
 				printPosition = x + offset;
 				if(printPosition%5 == 0){
 					writeChar(y, x, '1');
@@ -38,4 +39,5 @@ int main(){
 			offset = offset + unitChange;
 		}
 	}
+	return 0;
 }
