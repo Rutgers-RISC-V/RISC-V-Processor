d119cbb9bfa9b5ad2bab5f4fa830cbd98ceefdd7
Jimmy Wen <jimmy.z.wen@gmail.com>
2019-04-08 15:50:51 -0400

    Added demo to display all ASCII characters. Currently assumes we have the extended ASCII characters as well

---
+++ Terminal_Demos/RISC-V/AllChars/terminalDemoAllChars_Delay.s
@@ -0,0 +1,1293 @@
+# This program draws all available characters to terminal
+
+li x6 4096 # load base address of terminal into a register.
+
+li x7 0 # load an ASCII value into register
+
+# Output char to terminal
+addi x8 x6 0
+sb x7 0(x8)
+jalr x10 0(x9)
+
+#Print remaining standard characters
+li x7 1
+addi x8 x6 1
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 2
+addi x8 x6 2
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 3
+addi x8 x6 3
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 4
+addi x8 x6 4
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 5
+addi x8 x6 5
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 6
+addi x8 x6 6
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 7
+addi x8 x6 7
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 8
+addi x8 x6 8
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 9
+addi x8 x6 9
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 10
+addi x8 x6 10
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 11
+addi x8 x6 11
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 12
+addi x8 x6 12
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 13
+addi x8 x6 13
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 14
+addi x8 x6 14
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 15
+addi x8 x6 15
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 16
+addi x8 x6 16
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 17
+addi x8 x6 17
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 18
+addi x8 x6 18
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 19
+addi x8 x6 19
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 20
+addi x8 x6 20
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 21
+addi x8 x6 21
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 22
+addi x8 x6 22
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 23
+addi x8 x6 23
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 24
+addi x8 x6 24
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 25
+addi x8 x6 25
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 26
+addi x8 x6 26
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 27
+addi x8 x6 27
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 28
+addi x8 x6 28
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 29
+addi x8 x6 29
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 30
+addi x8 x6 30
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 31
+addi x8 x6 31
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 32
+addi x8 x6 32
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 33
+addi x8 x6 33
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 34
+addi x8 x6 34
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 35
+addi x8 x6 35
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 36
+addi x8 x6 36
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 37
+addi x8 x6 37
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 38
+addi x8 x6 38
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 39
+addi x8 x6 39
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 40
+addi x8 x6 40
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 41
+addi x8 x6 41
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 42
+addi x8 x6 42
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 43
+addi x8 x6 43
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 44
+addi x8 x6 44
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 45
+addi x8 x6 45
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 46
+addi x8 x6 46
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 47
+addi x8 x6 47
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 48
+addi x8 x6 48
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 49
+addi x8 x6 49
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 50
+addi x8 x6 50
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 51
+addi x8 x6 51
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 52
+addi x8 x6 52
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 53
+addi x8 x6 53
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 54
+addi x8 x6 54
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 55
+addi x8 x6 55
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 56
+addi x8 x6 56
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 57
+addi x8 x6 57
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 58
+addi x8 x6 58
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 59
+addi x8 x6 59
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 60
+addi x8 x6 60
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 61
+addi x8 x6 61
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 62
+addi x8 x6 62
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 63
+addi x8 x6 63
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 64
+addi x8 x6 64
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 65
+addi x8 x6 65
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 66
+addi x8 x6 66
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 67
+addi x8 x6 67
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 68
+addi x8 x6 68
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 69
+addi x8 x6 69
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 70
+addi x8 x6 70
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 71
+addi x8 x6 71
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 72
+addi x8 x6 72
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 73
+addi x8 x6 73
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 74
+addi x8 x6 74
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 75
+addi x8 x6 75
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 76
+addi x8 x6 76
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 77
+addi x8 x6 77
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 78
+addi x8 x6 78
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 79
+addi x8 x6 79
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 80
+addi x8 x6 80
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 81
+addi x8 x6 81
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 82
+addi x8 x6 82
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 83
+addi x8 x6 83
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 84
+addi x8 x6 84
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 85
+addi x8 x6 85
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 86
+addi x8 x6 86
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 87
+addi x8 x6 87
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 88
+addi x8 x6 88
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 89
+addi x8 x6 89
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 90
+addi x8 x6 90
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 91
+addi x8 x6 91
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 92
+addi x8 x6 92
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 93
+addi x8 x6 93
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 94
+addi x8 x6 94
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 95
+addi x8 x6 95
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 96
+addi x8 x6 96
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 97
+addi x8 x6 97
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 98
+addi x8 x6 98
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 99
+addi x8 x6 99
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 100
+addi x8 x6 100
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 101
+addi x8 x6 101
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 102
+addi x8 x6 102
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 103
+addi x8 x6 103
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 104
+addi x8 x6 104
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 105
+addi x8 x6 105
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 106
+addi x8 x6 106
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 107
+addi x8 x6 107
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 108
+addi x8 x6 108
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 109
+addi x8 x6 109
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 110
+addi x8 x6 110
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 111
+addi x8 x6 111
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 112
+addi x8 x6 112
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 113
+addi x8 x6 113
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 114
+addi x8 x6 114
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 115
+addi x8 x6 115
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 116
+addi x8 x6 116
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 117
+addi x8 x6 117
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 118
+addi x8 x6 118
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 119
+addi x8 x6 119
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 120
+addi x8 x6 120
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 121
+addi x8 x6 121
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 122
+addi x8 x6 122
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 123
+addi x8 x6 123
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 124
+addi x8 x6 124
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 125
+addi x8 x6 125
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 126
+addi x8 x6 126
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 127
+addi x8 x6 127
+sb x7 0(x8)
+jalr x10 0(x9)
+
+#Print extended ASCII characters
+li x7 128
+addi x8 x6 128
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 129
+addi x8 x6 129
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 130
+addi x8 x6 130
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 131
+addi x8 x6 131
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 132
+addi x8 x6 132
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 133
+addi x8 x6 133
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 134
+addi x8 x6 134
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 135
+addi x8 x6 135
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 136
+addi x8 x6 136
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 137
+addi x8 x6 137
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 138
+addi x8 x6 138
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 139
+addi x8 x6 139
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 140
+addi x8 x6 140
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 141
+addi x8 x6 141
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 142
+addi x8 x6 142
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 143
+addi x8 x6 143
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 144
+addi x8 x6 144
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 145
+addi x8 x6 145
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 146
+addi x8 x6 146
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 147
+addi x8 x6 147
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 148
+addi x8 x6 148
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 149
+addi x8 x6 149
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 150
+addi x8 x6 150
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 151
+addi x8 x6 151
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 152
+addi x8 x6 152
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 153
+addi x8 x6 153
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 154
+addi x8 x6 154
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 155
+addi x8 x6 155
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 156
+addi x8 x6 156
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 157
+addi x8 x6 157
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 158
+addi x8 x6 158
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 159
+addi x8 x6 159
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 160
+addi x8 x6 160
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 161
+addi x8 x6 161
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 162
+addi x8 x6 162
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 163
+addi x8 x6 163
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 164
+addi x8 x6 164
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 165
+addi x8 x6 165
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 166
+addi x8 x6 166
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 167
+addi x8 x6 167
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 168
+addi x8 x6 168
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 169
+addi x8 x6 169
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 170
+addi x8 x6 170
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 171
+addi x8 x6 171
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 172
+addi x8 x6 172
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 173
+addi x8 x6 173
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 174
+addi x8 x6 174
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 175
+addi x8 x6 175
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 176
+addi x8 x6 176
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 177
+addi x8 x6 177
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 178
+addi x8 x6 178
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 179
+addi x8 x6 179
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 180
+addi x8 x6 180
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 181
+addi x8 x6 181
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 182
+addi x8 x6 182
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 183
+addi x8 x6 183
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 184
+addi x8 x6 184
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 185
+addi x8 x6 185
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 186
+addi x8 x6 186
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 187
+addi x8 x6 187
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 188
+addi x8 x6 188
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 189
+addi x8 x6 189
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 190
+addi x8 x6 190
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 191
+addi x8 x6 191
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 192
+addi x8 x6 192
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 193
+addi x8 x6 193
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 194
+addi x8 x6 194
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 195
+addi x8 x6 195
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 196
+addi x8 x6 196
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 197
+addi x8 x6 197
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 198
+addi x8 x6 198
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 199
+addi x8 x6 199
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 200
+addi x8 x6 200
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 201
+addi x8 x6 201
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 202
+addi x8 x6 202
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 203
+addi x8 x6 203
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 204
+addi x8 x6 204
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 205
+addi x8 x6 205
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 206
+addi x8 x6 206
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 207
+addi x8 x6 207
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 208
+addi x8 x6 208
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 209
+addi x8 x6 209
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 210
+addi x8 x6 210
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 211
+addi x8 x6 211
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 212
+addi x8 x6 212
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 213
+addi x8 x6 213
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 214
+addi x8 x6 214
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 215
+addi x8 x6 215
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 216
+addi x8 x6 216
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 217
+addi x8 x6 217
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 218
+addi x8 x6 218
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 219
+addi x8 x6 219
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 220
+addi x8 x6 220
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 221
+addi x8 x6 221
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 222
+addi x8 x6 222
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 223
+addi x8 x6 223
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 224
+addi x8 x6 224
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 225
+addi x8 x6 225
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 226
+addi x8 x6 226
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 227
+addi x8 x6 227
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 228
+addi x8 x6 228
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 229
+addi x8 x6 229
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 230
+addi x8 x6 230
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 231
+addi x8 x6 231
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 232
+addi x8 x6 232
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 233
+addi x8 x6 233
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 234
+addi x8 x6 234
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 235
+addi x8 x6 235
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 236
+addi x8 x6 236
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 237
+addi x8 x6 237
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 238
+addi x8 x6 238
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 239
+addi x8 x6 239
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 240
+addi x8 x6 240
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 241
+addi x8 x6 241
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 242
+addi x8 x6 242
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 243
+addi x8 x6 243
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 244
+addi x8 x6 244
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 245
+addi x8 x6 245
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 246
+addi x8 x6 246
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 247
+addi x8 x6 247
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 248
+addi x8 x6 248
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 249
+addi x8 x6 249
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 250
+addi x8 x6 250
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 251
+addi x8 x6 251
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 252
+addi x8 x6 252
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 253
+addi x8 x6 253
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 254
+addi x8 x6 254
+sb x7 0(x8)
+jalr x10 0(x9)
+
+li x7 255
+addi x8 x6 255
+sb x7 0(x8)jalr x10 0(x9)
+
+frameBreak:
+lui x11 1024
+countdown:
+addi x11, x11, -1
+bne x11, x0, countdown
+jalr x0, 0(x10)
