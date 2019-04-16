efe1f74dd861b62025e219e341459574a48475c1
JonColella <jc2004@scarletmail.rutgers.edu>
2019-04-16 17:52:59 -0400

    Bug Fixes

--- rv32i_pipelined/rv32i_pipelined.runs/synth_1/RV32I_pipelined_wrapper.tcl
+++ rv32i_pipelined/rv32i_pipelined.runs/synth_1/RV32I_pipelined_wrapper.tcl
@@ -18,6 +18,7 @@ proc create_report { reportName command } {
   }
 }
 set_param xicom.use_bs_reader 1
+set_msg_config -id {Common 17-41} -limit 10000000
 create_project -in_memory -part xc7z010clg400-1
 
 set_param project.singleFileAddWarning.threshold 0
