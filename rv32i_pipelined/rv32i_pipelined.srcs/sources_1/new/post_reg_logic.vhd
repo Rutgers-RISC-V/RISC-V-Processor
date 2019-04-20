f6f05ebdf92bae9c288936a44f1b9c057f67c2a7
Oz Bejerano <oz.bejerano@rutgers.edu>
2019-04-20 14:29:09 -0400

    Increased size of memory from 16384 to 32768 4-byte words. Changed base address of terminal to 16384.

---
+++ rv32i_pipelined/rv32i_pipelined.srcs/sources_1/new/post_reg_logic.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/18/2019 10:01:03 PM
+-- Design Name: 
+-- Module Name: post_reg_logic - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity post_reg_logic is
+--  Port ( );
+end post_reg_logic;
+
+architecture Behavioral of post_reg_logic is
+
+begin
+
+
+end Behavioral;
